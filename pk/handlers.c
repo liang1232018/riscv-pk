4e34f94f93f0fa552bd077c53a51e39a7e4f11e1
preacle <lgy8@pku.edu.cn>
2017-11-27 15:41:37 +0800

    RV32E

--- pk/handlers.c
+++ pk/handlers.c
@@ -65,7 +65,7 @@ static void handle_fault_store(trapframe_t* tf)
 static void handle_syscall(trapframe_t* tf)
 {
   tf->gpr[10] = do_syscall(tf->gpr[10], tf->gpr[11], tf->gpr[12], tf->gpr[13],
-                           tf->gpr[14], tf->gpr[15], tf->gpr[17]);
+                           tf->gpr[14], tf->gpr[15], tf->gpr[5]);
   tf->epc += 4;
 }
 
